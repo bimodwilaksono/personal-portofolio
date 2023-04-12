26e0d167c201b6ff220eee7d505fba0869a500de
bimodwilaksono <bimodwilaksono@gmail.com>
2023-04-12 20:57:38 +0700

    Styling Skills page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { skills } from "@/data/Skills/skills";
-import { Box, Image, Tabs } from "@mantine/core";
+import { Box, Container, Flex, Image, Paper, Tabs, Title, Tooltip } from "@mantine/core";
 
 function Skills() {
     const categories = [...new Set(skills.map((el) => el.category))];
     const [activeTab, setActiveTab] = useState(categories[0]);
 
     return (
-        <>
-            <h1>Skills</h1>
+        <Container>
+            <Title>Skills</Title>
             <Tabs defaultValue={activeTab} onTabChange={setActiveTab}>
                 <Tabs.List>
                     {categories.map((category, index) => {
@@ -22,29 +22,53 @@ function Skills() {
 
                 {categories.map((category) => {
                     return (
-                        <Tabs.Panel value={category} key={category}>
-                            {skills
-                                .filter(
-                                    (skill) =>
-                                        skill.category.toLowerCase() === category.toLowerCase()
-                                )
-                                .map((skill) => (
-                                    <Box key={skill.name}>
-                                        <Image
-                                            src={skill.icon}
-                                            mah={100}
-                                            maw={100}
-                                            alt=''
-                                            withPlaceholder
-                                        />
-                                        <p>{skill.name}</p>
-                                    </Box>
-                                ))}
+                        <Tabs.Panel value={category} key={category} mt={"2rem"}>
+                            <Flex gap={"2rem"} justify={"center"} wrap={"wrap"}>
+                                {skills
+                                    .filter(
+                                        (skill) =>
+                                            skill.category.toLowerCase() === category.toLowerCase()
+                                    )
+                                    .map((skill) => (
+                                        <Tooltip
+                                            key={skill.name}
+                                            label={skill.name}
+                                            openDelay={500}
+                                            closeDelay={300}
+                                            position='bottom'
+                                            transitionProps={{
+                                                transition: "scale-y",
+                                            }}
+                                            events={{ hover: true, focus: true, touch: false }}
+                                            withArrow>
+                                            <Paper
+                                                w={200}
+                                                h={200}
+                                                shadow='lg'
+                                                radius={"xl"}
+                                                p='md'
+                                                withBorder
+                                                style={{
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    justifyContent: "center",
+                                                }}>
+                                                <Image
+                                                    src={skill.icon}
+                                                    mah={100}
+                                                    maw={100}
+                                                    alt=''
+                                                    withPlaceholder
+                                                />
+                                            </Paper>
+                                        </Tooltip>
+                                    ))}
+                            </Flex>
                         </Tabs.Panel>
                     );
                 })}
             </Tabs>
-        </>
+        </Container>
     );
 }
 
